22911caadd1d5ca58e082112cc88cd05eb02aaef
Thanh Pham <thanhpk@live.com>
2016-11-28 09:36:57 +0700

    add bootstrap

--- ecom/ui/src/main.js
+++ ecom/ui/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+
+import BootstrapVue from 'bootstrap-vue';
+
 import VueRouter from  'vue-router';
 import Home from './components/Home';
 import Hello from './components/Hello';
-
+Vue.use(BootstrapVue);
 Vue.use(VueRouter);
 
 const router = new VueRouter({
@@ -22,3 +25,4 @@ const router = new VueRouter({
 const app = new Vue({
 	router: router
 }).$mount('#app');
+
