ff053929552526a2767b28f755b27c3327c06821
Thanh Pham <thanhpk@live.com>
2016-11-28 23:34:30 +0700

    add jquery and fix home layout

--- ecom/ui/src/components/Home.vue
+++ ecom/ui/src/components/Home.vue
@@ -1,9 +1,288 @@
-<template lang="jade">
-	p Hello
-</template>
+<template >
+<div>
+ <div class="container"> 
+    <div class="header">
+      <div class="row animated bounce">
+        <div class="text-center mtverybig"><img class="logo-img " src="static/images/logo.png"/>
+          <p class="logo-text" style="font-family: 'Myriad Set Pro'; font-weight: 300">
+            <span style="color: #39576b;">s</span>
+            <span style="color: #39576b;">u</span>
+            <span style="color: #39576b;">t</span>
+            <span style="color: #39576b;">u</span>
+            .
+            <span style="color: #39576b;">s</span>
+            <span style="color: #39576b;">h</span>
+            <span style="color: #39576b;">o</span>
+            <span style="color: #39576b;">p</span>
+          </p>
+        </div>
+      </div>
+
+      <div class="row mtbig">
+        <ul class="navigation-menu mbmed">
+          <li><router-link to="/about">Make up</router-link></li>
+          <li><a>Skin care</a></li>
+					<li><a>Body care</a></li>
+					<li><a>Hair &amp; Teeth<a></li>
+          <li><a>Giỏ hàng: 4</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+	
+	<div class="container">
+		<div class="row ">
+			<carousel id="carousel-example-generic" class="carousel slide col-sm-12" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+          <div class="item active">
+            <img src="static/images/slide3.png" alt="...">
+            <div class="carousel-caption">
+              <h3>Caption Text</h3>
+            </div>
+          </div>
+          <div class="item">
+            <img src="static/images/slide1.png" alt="...">
+            <div class="carousel-caption">
+              <h3>Caption Text</h3>
+            </div>
+          </div>
+          <div class="item">
+            <img src="static/images/slide2.png" alt="...">
+            <div class="carousel-caption">
+              <h3>Caption Text</h3>
+            </div>
+          </div>
+        </div>
+
+
+      </carousel> <!-- Carousel -->
+
+
+    </div>
+
+  </div>
+	<div class="container">
+		<div class="row"><div class="col-xs-12 mtbig text-center"><h2>Chúng tôi đem đến cho bạn trải nghiệm mua sắm mỹ phẩm </h2></div></div>
+		<div class="row">
+			<div class="col-sm-4 text-center mtbig">
+				<i class="pe-7s-leaf feature-icon" style="color: #52b95c"></i>
+				<h3>Chỉ bán hàng chính hãng</h3>
+				<p>Gặp phải hàng fake? Chặt đầu tôi đi</p>
+			</div>
+			<div class="col-sm-4 text-center mtbig">
+				<i class="pe-7s-cash feature-icon" style="color: #e6c851"></i>
+				<h3>Miễn phí giao hàng</h3>
+				<p>Mua hàng là việc của bạn, giao hàng là việc của chúng tôi</p><p></p>
+			</div><div class="col-sm-4 text-center mtbig">
+				<i class="pe-7s-gift feature-icon" style="color: #b952a6"></i>
+				<h3>Miễn phí giao hàng</h3>
+				<p></p>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="container">
+		<div class="row">
+<div class="col-xs-12">
+<h2>Make up</h2>
+</div>
+			</div>
+		<div class="row">
+			<div class="col-sm-2 mtmed">
+				<img style="height: 100px; width: 100%"  src="http://www.goulko.com/media/thumb/clinique-happy-group2-srgb.jpg?w=300&h=187.5">
+<h5>Dau goi kho batiste</h5>
+			</div>
+			<div class="col-sm-2 mtmed">
+				<img style="height: 200px; width: 100%"  src="static/images/slide3.png">
+			</div>
+
+			<div class="col-sm-2 mtmed">
+				<img style="height: 200px; width: 100%"  src="static/images/slide3.png">
+			</div>
+			<div class="col-sm-2 mtmed">
+				<img style="height: 200px;  width: 100%" src="static/images/slide1.png">
+			</div>
+			<div class="col-sm-2 mtmed">
+				<img style="height: 200px; width: 100%"  src="static/images/slide2.png">
+			</div>
+			<div class="col-sm-2 mtmed">
+				<img style="height: 200px; width: 100%"  src="static/images/slide3.png">
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+<div class="mtbig text-center">
+<img src="http://vignette2.wikia.nocookie.net/animal-jam-clans-1/images/e/e0/Hipster_arrow_heart_page_divider.png/revision/latest?cb=20160729215711" />
+</div>
+			<div class="col-sm-12"><h2 class="text-center">San pham duoc yeu thich nhat</h2></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-3 mtmed">
+				<img style="height: 200px; width: 100%"  src="static/images/slide3.png">
+			</div>
+			<div class="col-sm-3 mtmed">
+				<img style="height: 200px;  width: 100%" src="static/images/slide1.png">
+			</div>
+			<div class="col-sm-3 mtmed">
+				<img style="height: 200px; width: 100%"  src="static/images/slide2.png">
+			</div>
+			<div class="col-sm-3 mtmed">
+				<img style="height: 200px; width: 100%"  src="static/images/slide3.png">
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+
+			<div class="col-sm-12"><h2 class="text-center mtbig">San pham moi ve</h2></div>
+<div class="text-center"><img src="http://vignette1.wikia.nocookie.net/animal-jam-clans-1/images/b/b8/Feature-wasp_art_-_section_divider.png/revision/latest?cb=20160701071439" /></div>
+
+		</div>
+
+		<div class="row">
+			<div class="col-sm-3 mtmed">
+				<img style="height: 200px; width: 100%"  src="static/images/slide3.png">
+			</div>
+			<div class="col-sm-3 mtmed">
+				<img style="height: 200px;  width: 100%" src="static/images/slide1.png">
+			</div>
+			<div class="col-sm-3 mtmed">
+				<img style="height: 200px; width: 100%"  src="static/images/slide2.png">
+			</div>
+			<div class="col-sm-3 mtmed">
+				<img style="height: 200px; width: 100%"  src="static/images/slide3.png">
+			</div>
+		</div>
+	</div>
+
+
+<div class="container">
+<div class="row">
+<div class="col-sm-12">
+<h2 class="text-center mtbig">Chung toi chi ban nhung do chung toi thay tot!</h2>
+
+</div>
+</div>
+
+<div class="row">
+<div class="col-sm-12">
+<div class="menu mtmed menu-makeup">
+<router-link to="/makeup">Make up</router-link>
+</div>
+
+</div>
+</div>
+
+<div class="row">
+<div class="col-sm-12">
+<div class="menu mtmed menu-skincare">
+<router-link to="/makeup">Skin care</router-link>
+</div>
+
+</div>
+</div>
+
+<div class="row">
+<div class="col-sm-12">
+<div class="menu menu-bodycare">
+<router-link to="/makeup">Body care</router-link>
+</div>
+
+</div>
+</div>
+
+<div class="row">
+<div class="col-sm-12">
+<div class="menu mtmed menu-hair">
+<router-link to="/makeup">Hair and teeth</router-link>
+</div>
+
+</div>
+</div>
+</div>
+<div class="container"></div>
+
+
+	<PageFooter></PageFooter>
+</div></template>
 
 <script>
+	import { carousel } from 'vue2-strap';
+	import PageFooter from './Footer.vue';
+	import $ from 'jquery';
+	console.log($('#app'));
+	console.log(document);
 export default {
-		
+	components: { carousel, PageFooter }	
 }
 	</script>
+
+<style>
+	.menu-skincare {
+	background: url('/static/images/skincare.jpg');
+	}
+	
+	.menu-makeup {
+		background: url('/static/images/makeup.jpg');
+	}
+
+	.menu-hair {
+	background: url('/static/images/hair.jpg');
+	}
+	.menu {
+	margin-top: 15px;
+	height: 100px;
+
+	-webkit-background-size:cover;
+-moz-background-size:cover;
+-o-background-size:cover;
+	background-size:cover;
+	backgorund-repeat: no-repeat;
+	}
+
+	.menu a:hover {
+	text-decoration: none;
+    text-shadow: 1px 1px 1px rgba(0,0,0,0.1);
+    color: rgba(255,255,255,0.9);
+	}
+
+	.menu a {
+	text-decorator: none;
+	width: 100%;
+	display: block;
+    font-weight: 300;
+    padding: 2%;
+    font-size: 66px;
+    letter-spacing: 7px;
+    text-shadow: 0px 0px 10px rgba(255,255,255,0.9);
+    color: rgba(255,255,255,0.1);
+ 
+    -webkit-background-size: 100%;
+    -moz-background-size: 100%;
+    -o-background-size: 100%;
+    background-size: 100%;
+    background-clip: padding-box;
+    -webkit-transition: all 0.3s ease-in-out;
+    -moz-transition: all 0.3s ease-in-out;
+    -o-transition: all 0.3s ease-in-out;
+    -ms-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
+}
+
+</style>
+
+
+
