a6a509dfee12caca72fba05aadd1384358d8203b
Thanh Pham <thanhpk@live.com>
2016-12-08 18:25:59 +0700

    add authentication

---
+++ ecom/auth/user.go
@@ -0,0 +1,24 @@
+package auth
+
+import (
+	bson "gopkg.in/mgo.v2/bson"
+	mgo "gopkg.in/mgo.v2"
+	commom_auth "github.com/thanhpk/sutu.shop/ecom/common/auth"
+)
+
+type UserMgr struct {
+	scope string
+	session mgo.Session
+	database string
+}
+
+func NewUserMgr(scope string) *UserMgr {
+	userMgr := &UserMgr{}
+
+	return userMgr
+
+}
+
+func (u UserMgr) MatchById(id bson.MongoId)  commom_auth.User {
+
+}
