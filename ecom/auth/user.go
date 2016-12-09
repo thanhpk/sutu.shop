b209f15a7dde86accf338b965abf4fffc8a5a096
Thanh Pham <thanhpk@live.com>
2016-12-09 18:42:46 +0700

    add findbyid

--- ecom/auth/user.go
+++ ecom/auth/user.go
@@ -1,25 +1,31 @@
 package auth
 
 import (
-	imgo "github.com/thanhpk/sutu.shop/ecom/common/db"
-	commom_auth "github.com/thanhpk/sutu.shop/ecom/common/auth"
+	mgo "gopkg.in/mgo.v2"
+	CA "github.com/thanhpk/sutu.shop/ecom/common/auth"
 )
 
 type UserMgr struct {
 	scope string
-	database imgo.Database
-	collection string
+	database mgo.Database
+	collectionName string
 }
 
-func NewUserMgr(scope string, database imgo.Database) *UserMgr {
+func NewUserMgr(scope string, database mgo.Database) *UserMgr {
 	userMgr := &UserMgr{
 		scope: scope,
 		database: database,
-		collection: scope + "_user"	}
+		collectionName: scope + "_user"	}
 	
 	return userMgr
 }
 
-func (u *UserMgr) MatchById(id string)  commom_auth.User {
-	
+func (u UserMgr) MatchById(id string) *CA.User {
+	c := u.database.C(u.collectionName)
+	user := CA.User{}
+	err := c.FindId(id).One(&user)
+	if err != nil {
+		return nil
+	}
+	return &user
 }
--- ecom/auth/user_test.go
+++ ecom/auth/user_test.go
@@ -1,11 +1,22 @@
 package auth
 
 import (
-	"fmt"
+//	"fmt"
 	mgo "gopkg.in/mgo.v2"
 	"testing"
 	imgo "github.com/thanhpk/sutu.shop/ecom/common/db"
 )
+
+type QueryMock struct {
+}
+
+func (c *CollectionMock) Find(query interface{}) *imgo.Query {
+	return nil
+}
+
+func (c *CollectionMock) FindId(id interface{}) *imgo.Query {
+	return nil
+}
 
 type MongoSessionMock struct {
 	mgo.Session
@@ -16,13 +27,18 @@ type DatabaseMock struct {
 
 type CollectionMock struct {}
 
-func (CollectionMock) EnsureIndexKey(key ...string) error {
+func (*CollectionMock) EnsureIndexKey(key ...string) error {
+	return nil
+}
+
+func (*CollectionMock) DropIndex(key ...string) error {
 	return nil
 }
 
 func (DatabaseMock) C(name string) *imgo.Collection {
-	c := imgo.Collection(CollectionMock{})
-	return &c
+//	c := imgo.Collection(CollectionMock{})
+	//return &c
+	return nil
 }
 
 func (s *MongoSessionMock) DB(name string) *imgo.Database {
@@ -31,8 +47,8 @@ func (s *MongoSessionMock) DB(name string) *imgo.Database {
 }
 
 func TestMatchById(t *testing.T) {
-	usermgr := NewUserMgr("111", DatabaseMock{})
+//	usermgr := NewUserMgr("111", DatabaseMock{})
 	
-	fmt.Println(usermgr)
+//	fmt.Println(usermgr)
 }
 
