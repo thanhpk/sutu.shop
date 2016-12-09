aaaca7029d8ab98ba2e6d6f1d8611d87bed966dd
Thanh Pham <thanhpk@live.com>
2016-12-09 18:42:58 +0700

    fix mock

--- ecom/common/db/mgo.go
+++ ecom/common/db/mgo.go
@@ -4,7 +4,22 @@ import (
 //	mgo "gopkg.in/mgo.v2"
 )
 
+type M map[string]interface{}
+
+type D []DocElem
+
+// See the D type.
+type DocElem struct {
+	Name  string
+	Value interface{}
+}
+
+type Query interface {
+}
+
 type Collection interface {
+	FindId(id interface{}) *Query
+	DropIndex(key ...string) error
 	EnsureIndexKey(key ...string) error 
 }
 
