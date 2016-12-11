a06caf7e82e95bcf39eb6147c9b917080aae6b1b
Thanh Pham <thanhpk@live.com>
2016-12-11 16:34:03 +0700

    add interface

--- ecom/usecase/login.go
+++ ecom/usecase/login.go
@@ -1,5 +1,27 @@
 package usecase
 
+import (
+	"time"
+)
+
+type AddressL struct {
+	Phone string
+	Address string
+}
+
+type CustomerL struct {
+	Id string
+	Name string
+		
+	Point int32
+	IsAdmin bool
+
+	Email string
+	Phone string
+	Addresses []AddressL
+}
+
 type ILogin interface {
-	Login(id string, password string)
+	AuthByPhone(phone string, password string) (*CustomerL, error)
+	AuthByFacebook(userid string, accessToken string) (*CustomerL, error)
 }
