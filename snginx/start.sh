2f40a8cbf264a3d8ff6dd741e2fe373e7929eac5
Thanh Pham <thanhpk@live.com>
2016-12-18 14:51:48 +0700

    add certbot

--- snginx/start.sh
+++ snginx/start.sh
@@ -1,5 +1,7 @@
-nginx -g 'daemon off;'
 
-letsencrypt certonly --webroot -w /usr/share/nginx/html -d sutu.shop -d blog.sutu.shop -d mail.sutu.shop -d admin.sutu.shop -d wwwstaging.sutu.shop -d adminstaging.sutu.shop -d api.sutu.shop --email hhnnaahhtt@gmail.com --agree-tos
+rm /etc/nginx/conf.d/default.conf
+nginx -g 'daemon off;' 1>>output.log 2>>error.log
 
-letsencrypt renew ---quiet --no-self-upgrade
+letsencrypt certonly --webroot -w /usr/share/nginx/html -d sutu.shop -d blog.sutu.shop -d mail.sutu.shop -d admin.sutu.shop -d wwwstaging.sutu.shop -d adminstaging.sutu.shop -d api.sutu.shop --email hhnnaahhtt@gmail.com --agree-tos 1>>output.log 2>>error.log
+
+letsencrypt renew --quiet --no-self-upgrade 1>>output.log 2>>error.log
