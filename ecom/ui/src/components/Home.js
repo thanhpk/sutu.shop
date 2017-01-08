ef2565a8d66a028ace88f876ab88ea53305f901c
Thanh Pham <thanhpk@live.com>
2017-01-08 19:23:24 +0700

    change home to vue

---
+++ ecom/ui/src/components/Home.js
@@ -0,0 +1,10 @@
+import bootstrapcss from 'bootstrap/css/bootstrap.min.css';
+import homecss from './Home.css';
+import carousel from 'bootstrap/carousel.vue';
+import dropdownhover from 'bootstrap/dropdownhover.vue';
+import PageFooter from './Footer.vue';
+
+
+export default {
+	components: { carousel, dropdownhover, PageFooter }
+};
