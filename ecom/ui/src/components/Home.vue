733becde5b6e980e1f880bd6acaad6996138db15
Thanh Pham <thanhpk@live.com>
2017-01-13 20:58:39 +0700

    fix home

--- ecom/ui/src/components/Home.vue
+++ ecom/ui/src/components/Home.vue
@@ -1,5 +1,20 @@
-<template src="./Home.jade" lang="jade"></template>
+<template src="./Home.jade" lang="jade" ></template>
 <script src="./Home.js"></script>
+<style>
+i.lgfb {
+  color: #3d578f;
+	margin-right: 15px;
+}
+
+i.lgfb:before {
+  font-size: 19px;
+	margin-left: 0px;
+}
+.menumore > ul > li {
+  text-transform: none;
+}
+	
+</style>
 
 
 
