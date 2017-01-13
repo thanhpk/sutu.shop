50982e4236b73c2e79f2c8582ab5800c34d1f33d
Thanh Pham <thanhpk@live.com>
2017-01-13 20:59:12 +0700

    relayout product

---
+++ ecom/ui/src/components/Product.js
@@ -0,0 +1,10 @@
+import bootstrapcss from 'bootstrap/css/bootstrap.min.css';
+import homecss from './Home.css';
+import carousel from 'bootstrap/carousel.vue';
+import dropdownhover from 'bootstrap/dropdownhover.vue';
+import FunButton from './FunButton.vue';
+
+
+export default {
+	components: { funbutton: FunButton }
+};
